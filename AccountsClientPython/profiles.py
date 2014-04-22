71f378450b3f75b473a206b44a700be4807e8f54
techkid6 <evan.markowitz99@gmail.com>
2014-04-22 03:01:08 +0100

    this doesn't work

--- AccountsClientPython/profiles.py
+++ AccountsClientPython/profiles.py
@@ -5,21 +5,26 @@ PROFILES_URL = "https://api.mojang.com/profiles/minecraft"
 SESSIONS_URL = "https://sessionserver.mojang.com/session/minecraft/profile/{uuid}"
 
 def find_profiles_by_names(*names):
-  headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
-  body = json.dumps(names)
-  result = post(PROFILES_URL, body, headers)
+  result = []
+  pages = [ names[i:i+100] for i in range(0, len(names), 100) ]
+  print pages
+  for page in pages:
+    headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
+    body = json.dumps(page)
+    print body+"\n"
+    result += _post(PROFILES_URL, body, headers)
   return result
 
 def find_profiles_by_uuids(*uuids):
   profiles = []
   for uuid in uuids:
-    profiles.append(get(SESSIONS_URL.format(uuid=uuid)))
+    profiles.append(_get(SESSIONS_URL.format(uuid=uuid)))
   return profiles
 
-def post(url, body, head):
+def _post(url, body, head):
   response = requests.post(url, data=body, headers=head)
   return response.json()
 
-def get(url):
+def _get(url):
   response = requests.get(url)
   return response.json()
