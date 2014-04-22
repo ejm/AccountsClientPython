fd4e6b854329f38b8b99fb7aaa77f1e38b490e46
techkid6 <evan.markowitz99@gmail.com>
2014-04-22 03:27:02 +0100

    0.3, fixed #1, updated documentation

--- AccountsClientPython/profiles.py
+++ AccountsClientPython/profiles.py
@@ -4,22 +4,34 @@ import json
 PROFILES_URL = "https://api.mojang.com/profiles/minecraft"
 SESSIONS_URL = "https://sessionserver.mojang.com/session/minecraft/profile/{uuid}"
 
-def find_profiles_by_names(*names):
+def find_profiles_by_names(names):
   result = []
   pages = [ names[i:i+100] for i in range(0, len(names), 100) ]
-  print pages
   for page in pages:
     headers = {'Content-type': 'application/json', 'Accept': 'application/json'}
     body = json.dumps(page)
-    print body+"\n"
     result += _post(PROFILES_URL, body, headers)
   return result
+  
+def find_profile_by_name(name):
+  result = find_profiles_by_names([name])
+  if result[0] is None:
+    return None
+  else:
+    return result[0]
 
-def find_profiles_by_uuids(*uuids):
+def find_profiles_by_uuids(uuids):
   profiles = []
   for uuid in uuids:
     profiles.append(_get(SESSIONS_URL.format(uuid=uuid)))
   return profiles
+  
+def find_profile_by_uuid(uuid):
+  result = find_profiles_by_uuids([uuid])
+  if result[0] is None:
+    return None
+  else:
+    return result[0]
 
 def _post(url, body, head):
   response = requests.post(url, data=body, headers=head)
--- README.md
+++ README.md
@@ -10,27 +10,12 @@ Installation
 pip install git+git://github.com/techkid6/AccountsClientPython
 ```
 
-Example Usage
--------------
+Examples
+--------
 
-```python
-# This gets the UUID of a player by name
-from AccountsClientPython.profiles import find_profiles_by_names
-
-profiles = find_profiles_by_names('techkid6')
-if profiles is not None and len(profiles) > 0:
-    print profiles[0]['id']
-```
-
-```python
-# This gets the name of a player by UUID
-from AccountsClientPython.profiles import find_profiles_by_uuids
-
-profiles = find_profiles_by_uuids('d560431ed516419580885b294f4f83f0')
-if profiles is not None and len(profiles) > 0:
-    print profiles[0]['name']
-````
+You can find examples in ```examples.py```
 
 License
 -------
+
 This project is a fork of [this project](https://github.com/Mojang/AccountsClient), which has no written license other than the fact that one may fork and convert to other languages as they please.  I am keeping with this license
