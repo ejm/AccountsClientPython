d754caca9a0a73798a2d9a9cbe3a44bbc0abb9ac
Evan Markowitz <evan.markowitz99@gmail.com>
2015-09-12 11:19:30 -0400

    I think this has been broken for a while

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name='AccountsClientPython',
   version='0.3',
   description="A python version of Mojang's AccountsClient repository with some extra features",
-  install_requires=['requests','json'],
+  install_requires=['requests'],
   author='techkid6',
   author_email='evan.markowitz99@gmail.com',
   packages=['AccountsClientPython']
