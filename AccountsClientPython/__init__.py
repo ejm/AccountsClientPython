f5f82cdad45c6a09ee2dbfaeee9d9872f38914e0
Evan Markowitz <evan.markowitz99@gmail.com>
2018-01-25 22:21:41 -0500

    Update __init__.py
    I wonder how long this has been broken?

--- AccountsClientPython/__init__.py
+++ AccountsClientPython/__init__.py
@@ -1,13 +1,13 @@
 import profiles
 
 def by_name(name):
-  profiles.find_profile_by_name(name)
+  return profiles.find_profile_by_name(name)
 
 def by_uuid(uuid):
-  profiles.find_profile_by_uuid(uuid)
+  return profiles.find_profile_by_uuid(uuid)
 
 def name_history(name):
-  profiles.find_history_by_name(name)
+  return profiles.find_history_by_name(name)
 
 def uuid_history(uuid):
-  profiles.find_history_by_uuid(uuid)
+  return profiles.find_history_by_uuid(uuid)
