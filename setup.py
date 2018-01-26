7f8286a17527489f9915d4328b488c4137ec771c
Evan Markowitz <evan.markowitz99@gmail.com>
2018-01-25 22:22:10 -0500

    Bump version number for __init__ fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='AccountsClientPython',
-  version='0.4',
+  version='0.4.1',
   description="A python version of Mojang's AccountsClient repository with some extra features",
   install_requires=['requests'],
   author='techkid6',
