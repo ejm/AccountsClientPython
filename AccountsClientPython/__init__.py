7fb21beea432a7bfea590daf7d6f18babda30a0c
hube12 <h66R10@gmail.com>
2019-01-03 22:50:38 +0100

    Changed a few methods and made it easier to use

--- AccountsClientPython/__init__.py
+++ AccountsClientPython/__init__.py
@@ -1,14 +1,21 @@
-from .profiles import find_profile_by_name,find_profile_by_uuid,find_history_by_name,find_history_by_uuid
+from .profiles import *
 #relative import is needed as sys.path might not have this directory in it albeit it has site-package
 
 def by_name(name):
-  return profiles.find_profile_by_name(name)
+  return find_profile_by_name(name)
 
 def by_uuid(uuid):
-  return profiles.find_profile_by_uuid(uuid)
+  return find_profile_by_uuid(uuid)
 
 def name_history(name):
-  return profiles.find_history_by_name(name)
+  return find_history_by_name(name)
 
 def uuid_history(uuid):
-  return profiles.find_history_by_uuid(uuid)
+  return find_history_by_uuid(uuid)
+  
+def by_names(name):
+  return find_profile_by_names(name)
+  
+def by_uuids(uuid):
+  return find_profile_by_uuids(uuid)
+  
